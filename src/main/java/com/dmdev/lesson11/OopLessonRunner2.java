ccf243bf34447c39c8bf40ef77d418e8415d4919
Viner108 <andrukovichl@mail.ru>
2023-05-14 18:26:14 +0300

    video 11(принципы ООП) commit

--- src/main/java/com/dmdev/lesson11/OopLessonRunner2.java
+++ src/main/java/com/dmdev/lesson11/OopLessonRunner2.java
@@ -4,7 +4,7 @@ public class OopLessonRunner2 {
     public static void main(String[] args) {
         Printable laptop = new Laptop(new Ssd(250), new Ram(1024), 2);
         Printable mobile = new Mobile(new Ssd(125), new Ram(512));
-        print(laptop,mobile);
+        printWithRandom(laptop,mobile);
     }
     public static void print(Printable...objects){
         for (Printable object:objects){
@@ -12,4 +12,11 @@ public class OopLessonRunner2 {
             System.out.println();
         }
     }
+    public static void printWithRandom(Printable...objects){
+        for (Printable object:objects){
+            object.printWithRandom();
+            System.out.println();
+        }
+    }
+
 }
